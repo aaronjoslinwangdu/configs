b4f35c5fc873b3617f206e61b95449a2e626dc69
aaronjoslinwangdu <aaronjoslinwangdu@gmail>
2025-04-06 16:35:30 -0400

    update alpha dashboard

--- .config/nvim/lua/plugins/alpha.lua
+++ .config/nvim/lua/plugins/alpha.lua
@@ -1,10 +1,34 @@
--- [https://github.com/goolord/alpha-nvim]
 return {
   "goolord/alpha-nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
+    local alpha = require("alpha")
     local startify = require("alpha.themes.startify")
     startify.file_icons.provider = "devicons"
-    require("alpha").setup(startify.config)
+
+    startify.section.header.val = {
+      [[                            ]],
+      [[                            ]],
+      [[                            ]],
+      [[                            ]],
+      [[                            ]],
+      [[                            ]],
+      [[⠀⠀⠀⠀⠀⠀⢀⡤⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡀⠀⠀⠀⠀⠀⠀]],
+      [[⠀⠀⠀⠀⠀⢀⡏⠀⠀⠈⠳⣄⠀⠀⠀⠀⠀⣀⠴⠋⠉⠉⡆⠀⠀⠀⠀⠀]],
+      [[⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀⠈⠉⠉⠙⠓⠚⠁⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀]],
+      [[⠀⠀⠀⠀⢀⠞⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣄⠀⠀⠀⠀]],
+      [[⠀⠀⠀⠀⡞⠀⠀⠀⠀⠀⠶⠀⠀⠀⠀⠀⠀⠦⠀⠀⠀⠀⠀⠸⡆⠀⠀⠀]],
+      [[⢠⣤⣶⣾⣧⣤⣤⣀⡀⠀⠀⠀⠀⠈⠀⠀⠀⢀⡤⠴⠶⠤⢤⡀⣧⣀⣀⠀]],
+      [[⠻⠶⣾⠁⠀⠀⠀⠀⠙⣆⠀⠀⠀⠀⠀⠀⣰⠋⠀⠀⠀⠀⠀⢹⣿⣭⣽⠇]],
+      [[⠀⠀⠙⠤⠴⢤⡤⠤⠤⠋⠉⠉⠉⠉⠉⠉⠉⠳⠖⠦⠤⠶⠦⠞⠁⠀⠀]],
+    }
+
+    local header_width = 32
+    local term_width = vim.o.columns
+    startify.config.opts.margin = math.floor((term_width - header_width) / 2)
+    startify.config.opts.redraw_on_resize = true
+
+    alpha.setup(startify.config)
   end,
 }
+
